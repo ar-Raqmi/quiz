d2a7adc525fff2212ab97976738f8895e355eef6
ar-Raqmi <ar-raqmi@proton.me>
2026-05-02 15:43:08 +0800

    fix: Set base path for GitHub Pages correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  const isGitHubPages = env.VITE_GITHUB_PAGES === 'true';
-  const repoName = env.VITE_GITHUB_REPO;
+  const isGitHubPages = process.env.VITE_GITHUB_PAGES === 'true';
+  const repoName = process.env.VITE_GITHUB_REPO;
   return {
     plugins: [react(), tailwindcss()],
     base: isGitHubPages ? `/${repoName}/` : '/',
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
